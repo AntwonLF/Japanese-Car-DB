5e458d3a745ad1ce3c95e954e71b508d54594ce0
Antwon Frager Jr <Antwon.frager@gmail.com>
2024-01-06 14:47:31 -0500

    like button implementation

--- models/Car.js
+++ models/Car.js
@@ -6,7 +6,11 @@ const carSchema = new Schema({
     make: String,
     model: String,
     year: Number,
-    picture: String 
+    picture: String,
+    likes: {
+        type: Number,
+        default: 0
+    }
 })
 
 const Car = mongoose.model('Car', carSchema)
