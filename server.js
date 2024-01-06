2dfdf8822c392c0683c9dd19a95bfd0eb5342b65
Antwon Frager Jr <Antwon.frager@gmail.com>
2024-01-06 13:56:07 -0500

    cleaned up code

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
+// Import statements
 import express from 'express';
 import mongoose from 'mongoose';
 import session from 'express-session';
 import cors from 'cors';
-import router  from './routes/index.js'; 
-import './config/database.js'; 
-import 'dotenv/config.js';
-import connectDB from './config/database.js';
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
 import MongoStore from 'connect-mongo';
+import dotenv from 'dotenv/config'; // Considered best practice to import as 'dotenv/config'
+import connectDB from './config/database.js';
+import router from './routes/index.js'; 
+import './config/database.js'; 
 
-
-
+// Initialize express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Static files
 app.use(express.static('front-end'));
+
+// CORS configuration
 const corsOptions = {
     origin: '*', 
     credentials: true 
 };
+app.use(cors(corsOptions));
+
+// Body Parser Middleware
 app.use(express.json());
+
+// Error Handling Middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
-   });
-
-   
+});
 
 // Session configuration
 app.use(session({
@@ -33,26 +39,25 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        mongoUrl: 'mongodb+srv://Test1234:Test1234@cluster0.16dhpdw.mongodb.net/Japanes-Car-API?retryWrites=true&w=majority',
+        mongoUrl: process.env.MONGO_URI, 
         collectionName: 'sessions'
     }),
-    cookie: { 
+    cookie: {
         secure: false,
-    maxAge: 1000 * 60 * 60 * 24
+        maxAge: 1000 * 60 * 60 * 24 // 1 day
     }
 }));
 
-// Use the routes
-app.use('/api',cors(corsOptions), router);
-// app.use(cors(corsOptions));
+// API routes
+app.use('/api', router);
+
+// Redirect root to /api/cars
 app.get('/', (req, res) => {
     res.redirect('/api/cars');
 });
 
-
+// Start server and connect to DB
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-
 connectDB();
